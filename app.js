341b648d4e6c44302bac1c9c0643fd6aaa331dcf
youssif hany <youssifhany75@gmail.com>
2021-10-30 22:17:09 +0200

    add custom-header

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
-const express=require("express");
-const app=express();
-const PORT=3000;
+const express = require("express");
+const app = express();
+const PORT = 3000;
 
-app.get("/",(req,res)=>{
-	res.send("Holla");
+app.get("/", (req, res) => {
+  res.send("Holla");
 });
 
-app.listen(PORT,()=>console.log(`running on port${PORT}...`))
+app.get("/demo", (req, res) => {
+  res
+    .set("X-CUSTOM-HEADER", "Bang Bang!")
+    .status(418)
+    .send("LOOK AT NETWORK TAP");
+});
+
+app.listen(PORT, () => console.log(`running on port${PORT}...`));
