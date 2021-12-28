4c3aa52ddeb2c282cf4c04d3d2a76bfac8461d1c
youssif hany <youssifhany75@gmail.com>
2021-12-28 05:04:25 +0200

    add browser-colors

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import { useContext } from "react";
 
-import About from "./components/about/About";
+// import About from "./components/about/About";
 import Contact from "./components/contact/Contact";
 import Intro from "./components/intro/Intro";
 import ProductList from "./components/productList/ProductList";
