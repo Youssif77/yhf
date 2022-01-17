4158ad49b9f27a9906bf7e4397b0b8c44b5d7322
youssif hany <youssifhany75@gmail.com>
2022-01-17 22:02:02 +0200

    replace colorlib-with-natours

--- client/src/data.js
+++ client/src/data.js
@@ -2,14 +2,15 @@ import YourPlaces from "./img/your-places.png";
 import Forkify from "./img/forkify.png";
 import Gpt3 from "./img/gpt3.png";
 import FrontendBootcamp from "./img/frontend-bootcamp.png";
-import Colorlib from "./img/colorlib.png";
+// import Colorlib from "./img/colorlib.png";
 import Tasuku from "./img/tasuku.png";
+import Natours from "./img/natours.png";
 
 export const products = [
   {
     id: 1,
-    img: Forkify,
-    link: "https://forkify-yhf.netlify.app/",
+    img: Natours,
+    link: "https://natours-yhf.herokuapp.com/",
   },
   {
     id: 2,
@@ -18,8 +19,8 @@ export const products = [
   },
   {
     id: 3,
-    img: Gpt3,
-    link: "https://gpt3-lovat.vercel.app/",
+    img: Forkify,
+    link: "https://forkify-yhf.netlify.app/",
   },
   {
     id: 4,
@@ -28,12 +29,12 @@ export const products = [
   },
   {
     id: 5,
-    img: Colorlib,
-    link: "https://colorlib-yhf.netlify.app/",
+    img: Gpt3,
+    link: "https://gpt3-lovat.vercel.app/",
   },
   {
     id: 6,
     img: YourPlaces,
-    link: "https://yourPlaces-332222.web.app/",
+    link: "https://your-places-332222.web.app/",
   },
 ];
